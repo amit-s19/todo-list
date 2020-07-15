e271fe038114300155ddeef44e47456d3db9fb50
Satan <coutamit.s19@gmail.com>
2020-07-15 21:14:47 +0530

    Updates

---
+++ precache-manifest.3127d298ff88dde312625cb43dcd932b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2531063744441a3bce8a1f77f43eb721",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "d8dd012ae4cbff4e80f3",
+    "url": "/todo-list/static/css/main.ddec892e.chunk.css"
+  },
+  {
+    "revision": "3114309e506955336fe7",
+    "url": "/todo-list/static/js/2.7a9b1908.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.7a9b1908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8dd012ae4cbff4e80f3",
+    "url": "/todo-list/static/js/main.80d1be87.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
